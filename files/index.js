45992be27d7be6778dafd1ccecee88f307958c32
Sepidehri <sepidriahi@gmail.com>
2023-06-21 18:59:46 +0200

    a lot of css and html changes

--- files/index.js
+++ files/index.js
@@ -1,54 +1,35 @@
-$('.zodiac-sign').on('click', function(){
-  // Get the sign from the clicked button
-  const sign = $(this).data('sign');
+$(function() {
+  $('.zodiac-sign').on('click', function(){
+    // Get the sign from the clicked button
+    const sign = $(this).data('sign');
   
-  // Make a request to your server's astrology endpoint
-  fetch('/astrology', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      sign: sign,
-      day: 'today'
+    // Make a request to your server's astrology endpoint
+    fetch(`/astrology/${sign}/today`)
+    .then(response => response.json())
+    .then(data => {
+      // Redirect to a new page and pass the horoscope data
+      window.location.href = '/horoscope.html?data=' + encodeURIComponent(JSON.stringify(data));
     })
-  })
-  .then(response => response.json())
-  .then(data => {
-    // Redirect to a new page and pass the horoscope data
-    window.location.href = '/horoscope.html?data=' + encodeURIComponent(JSON.stringify(data));
-  })
-  .catch(error => {
-    console.error(error);
+    .catch(error => {
+      console.error(error);
+    });
   });
 });
 
-
-    fetch(API_URL, options)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        alert(JSON.stringify(data));
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  
-
-
-$('.zodiac-sign').on('click', function(){
-  // Get the sign from the clicked button
-  const sign = $(this).data('sign');
-  
-  // Make a request to your server's astrology endpoint
-  fetch(`/astrology/${sign}/today`)
-  .then(response => response.json())
-  .then(data => {
-      // Redirect to a new page and pass the horoscope data
-      window.location.href = '/horoscope.html?data=' + encodeURIComponent(JSON.stringify(data));
-  })
-  .catch(error => {
-      console.error(error);
+document.addEventListener('DOMContentLoaded', (event) => {
+  document.querySelectorAll('.zodiac-sign').forEach(button => {
+    button.addEventListener('click', function() {
+      const sign = this.getAttribute('data-sign');
+      
+      fetch(`/astrology/${sign}/today`)
+        .then(response => response.json())
+        .then(data => {
+          window.location.href = '/horoscope.html?data=' + encodeURIComponent(JSON.stringify(data));
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    });
   });
 });
 
--- server.js
+++ server.js
@@ -3,53 +3,46 @@ const app = express();
 const path = require("path");
 const http = require("http");
 const bodyParser = require("body-parser");
+const axios = require('axios');
 
-// Serve static content in directory 'files'
 app.use(express.static(path.join(__dirname, "files")));
-
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
-  });
+});
 
 app.get('/header', (req, res) => {
     res.sendFile(__dirname + '/header.html');
-  });
+});
 
-  app.get('/footer', (req, res) => {
+app.get('/footer', (req, res) => {
     res.sendFile(__dirname + '/footer.html');
-  }); 
+});
 
-  app.post('/astrology', async (req, res) => {
-    const sign = req.body.sign;
-    const day = req.body.day;
-  
+app.get('/astrology/:sign/today', async (req, res) => {
+    const sign = req.params.sign;
+    const day = 'today';
+
     const options = {
-      method: 'POST',
-      url: 'https://sameer-kumar-aztro-v1.p.rapidapi.com/',
-      params: {
-        sign: sign,
-        day: day
-      },
+      method: 'GET',
+      url: `https://sameer-kumar-aztro-v1.p.rapidapi.com/?sign=${sign}&day=${day}`,
       headers: {
-        'X-RapidAPI-Key': '6c588273dbmsh5e380d6f76000dep1312dfjsnfd5b6c1e7f5b',
+        'X-RapidAPI-Key':  '6c588273dbmsh5e380d6f76000dep1312dfjsnfd5b6c1e7f5b',
         'X-RapidAPI-Host': 'sameer-kumar-aztro-v1.p.rapidapi.com'
       }
     };
-    
+
     try {
       const response = await axios.request(options);
       console.log(response.data);
-  
-      // Send the response data back to the frontend
       res.json(response.data);
     } catch (error) {
       console.error(error);
       res.status(500).send('Server Error');
     }
-  });
+});
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
-  })
+})
