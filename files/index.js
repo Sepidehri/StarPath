ede747e65ab62cf8d2d9a43e3a2ae47ed2b4e33d
Sepidehri <sepidriahi@gmail.com>
2023-06-21 13:47:55 +0200

    api is connected

--- files/index.js
+++ files/index.js
@@ -1,36 +1,54 @@
-$(function() {
+$('.zodiac-sign').on('click', function(){
+  // Get the sign from the clicked button
+  const sign = $(this).data('sign');
   
-  $('.zodiac-sign').on('click', async function(){
-    alert(1)
-  
-  // Make a request to your vedic astrology API to get the horoscope for the sign
-  const API_URL = 'https://sameer-kumar-aztro-v1.p.rapidapi.com/';
-  const axios = require('axios');
-  alert(2)
-  const options = {
+  // Make a request to your server's astrology endpoint
+  fetch('/astrology', {
     method: 'POST',
-    url: API_URL,
-    params: {
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
       sign: sign,
       day: 'today'
-    },
-    headers: {
-      'X-RapidAPI-Key': '5e0a055384msha3dcec8bcf7f985p119cf3jsn8103cce6b196',
-      'X-RapidAPI-Host': 'sameer-kumar-aztro-v1.p.rapidapi.com'
-    }
-  };
-  
-  try {
-    alert(3)
-    // Make the API request using axios
-    const response = await axios.request(options);
-    console.log(response.data);
-    
-    // Replace the implementation below with your desired method of displaying the data
-    alert(response.data);
-  } catch (error) {
+    })
+  })
+  .then(response => response.json())
+  .then(data => {
+    // Redirect to a new page and pass the horoscope data
+    window.location.href = '/horoscope.html?data=' + encodeURIComponent(JSON.stringify(data));
+  })
+  .catch(error => {
     console.error(error);
-  }
+  });
+});
+
+
+    fetch(API_URL, options)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        alert(JSON.stringify(data));
+      })
+      .catch(error => {
+        console.error(error);
+      });
   
+
+
+$('.zodiac-sign').on('click', function(){
+  // Get the sign from the clicked button
+  const sign = $(this).data('sign');
+  
+  // Make a request to your server's astrology endpoint
+  fetch(`/astrology/${sign}/today`)
+  .then(response => response.json())
+  .then(data => {
+      // Redirect to a new page and pass the horoscope data
+      window.location.href = '/horoscope.html?data=' + encodeURIComponent(JSON.stringify(data));
+  })
+  .catch(error => {
+      console.error(error);
   });
-  });
+});
+
--- server.js
+++ server.js
@@ -21,6 +21,34 @@ app.get('/header', (req, res) => {
     res.sendFile(__dirname + '/footer.html');
   }); 
 
+  app.post('/astrology', async (req, res) => {
+    const sign = req.body.sign;
+    const day = req.body.day;
+  
+    const options = {
+      method: 'POST',
+      url: 'https://sameer-kumar-aztro-v1.p.rapidapi.com/',
+      params: {
+        sign: sign,
+        day: day
+      },
+      headers: {
+        'X-RapidAPI-Key': '6c588273dbmsh5e380d6f76000dep1312dfjsnfd5b6c1e7f5b',
+        'X-RapidAPI-Host': 'sameer-kumar-aztro-v1.p.rapidapi.com'
+      }
+    };
+    
+    try {
+      const response = await axios.request(options);
+      console.log(response.data);
+  
+      // Send the response data back to the frontend
+      res.json(response.data);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Server Error');
+    }
+  });
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
