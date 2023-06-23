6bba5fa5d88f4c069e3cea3367c1fd711e0419e6
Sepidehri <sepidriahi@gmail.com>
2023-06-23 15:46:29 +0200

    a lot of changes regarding jwt

--- server.js
+++ server.js
@@ -1,23 +1,36 @@
 const express = require('express');
 const app = express();
-const path = require("path");
-const http = require("http");
-const bodyParser = require("body-parser");
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
 const axios = require('axios');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
-app.use(express.static(path.join(__dirname, "files")));
+// Dummy user data for testing purposes
+const users = [
+  {
+    id: 1,
+    username: 'example',
+    password: '$2b$10$grR2g4aOUEM87fTE2gOhSeojN6/5ETPLrqBCLWQFdKbc/fkIn/tm6', // Hashed password for 'password'
+    name: 'John Doe',
+    email: 'john.doe@example.com',
+  },
+];
+
+app.use(express.static(path.join(__dirname, 'files')));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/header', (req, res) => {
-    res.sendFile(__dirname + '/header.html');
+  res.sendFile(__dirname + '/header.html');
 });
 
 app.get('/footer', (req, res) => {
-    res.sendFile(__dirname + '/footer.html');
+  res.sendFile(__dirname + '/footer.html');
 });
 
 app.get('/register', (req, res) => {
@@ -38,13 +51,12 @@ app.get('/account', (req, res) => {
 
 app.get('/chart', (req, res) => {
   res.sendFile(__dirname + '/chart-analysis.html');
-}); 
+});
 
 app.get('/horoscope/:sign', (req, res) => {
   const sign = req.params.sign;
   res.sendFile(__dirname + `/horoscope.html?sign=${sign}`);
 });
-
 
 app.get('/charts-calculations', (req, res) => {
   res.sendFile(__dirname + '/charts-calculations.html');
@@ -55,28 +67,124 @@ app.get('/matching-compatibility', (req, res) => {
 });
 
 app.get('/astrology/:sign/today', async (req, res) => {
-    const sign = req.params.sign;
-    const day = 'today';
+  const sign = req.params.sign;
+  const day = 'today';
 
-    const options = {
-      method: 'GET',
-      url: `https://sameer-kumar-aztro-v1.p.rapidapi.com/?sign=${sign}&day=${day}`,
-      headers: {
-        'X-RapidAPI-Key':  '6c588273dbmsh5e380d6f76000dep1312dfjsnfd5b6c1e7f5b',
-        'X-RapidAPI-Host': 'sameer-kumar-aztro-v1.p.rapidapi.com'
-      }
-    };
+  const options = {
+    method: 'GET',
+    url: `https://sameer-kumar-aztro-v1.p.rapidapi.com/?sign=${sign}&day=${day}`,
+    headers: {
+      'X-RapidAPI-Key': '6c588273dbmsh5e380d6f76000dep1312dfjsnfd5b6c1e7f5b',
+      'X-RapidAPI-Host': 'sameer-kumar-aztro-v1.p.rapidapi.com',
+    },
+  };
 
-    try {
-      const response = await axios.request(options);
-      console.log(response.data);
-      res.json(response.data);
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('Server Error');
-    }
+  try {
+    const response = await axios.request(options);
+    console.log(response.data);
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server Error');
+  }
 });
+
+app.post('/register', async (req, res) => {
+  const { username, password, name, email } = req.body;
+
+  // Check if the username is already taken
+  const existingUser = users.find((user) => user.username === username);
+  if (existingUser) {
+    return res.status(400).json({ message: 'Username already exists' });
+  }
+
+  // Hash the password
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  // Create a new user object
+  const user = {
+    id: users.length + 1,
+    username,
+    password: hashedPassword,
+    name,
+    email,
+  };
+
+  // Store the user in the users array (replace with database storage in production)
+  users.push(user);
+
+  // Generate a JWT token
+  const token = jwt.sign({ username: user.username }, 'b478b865481d0b890145b674ffb5d2d69dfc63316730f5023331b25d1149060ca64e176dd91cb67cb461b995c7b97cf6b5b8bfb3927c50427a60d725f856e07c', {
+    expiresIn: '1h', // Set the expiration time for the token
+  });
+
+  res.json({ token });
+});
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+
+  // Find the user by username
+  const user = users.find((user) => user.username === username);
+
+  if (!user) {
+    return res.status(401).json({ message: 'Invalid username or password' });
+  }
+
+  // Compare the provided password with the stored hashed password
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordValid) {
+    return res.status(401).json({ message: 'Invalid username or password' });
+  }
+
+  // Generate a JWT token
+  const token = jwt.sign({ username: user.username }, 'b478b865481d0b890145b674ffb5d2d69dfc63316730f5023331b25d1149060ca64e176dd91cb67cb461b995c7b97cf6b5b8bfb3927c50427a60d725f856e07c', {
+    expiresIn: '1h', // Set the expiration time for the token
+  });
+
+  res.json({ token });
+});
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.sendStatus(401); // No token provided
+  }
+
+  jwt.verify(token, 'b478b865481d0b890145b674ffb5d2d69dfc63316730f5023331b25d1149060ca64e176dd91cb67cb461b995c7b97cf6b5b8bfb3927c50427a60d725f856e07c', (err, user) => {
+    if (err) {
+      return res.sendStatus(403); // Invalid token
+    }
+
+    req.user = user; // Attach the decoded user information to the request object
+    next();
+  });
+}
+
+
+app.get('/account', authenticateToken, (req, res) => {
+  const username = req.user.username;
+
+  // Find the user by username in the users array
+  const user = users.find((user) => user.username === username);
+
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+
+  res.json({ user: { firstName: user.firstName, lastName: user.lastName, email: user.email } });
+});
+
+
+
+
+
+
+
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
-})
+});
