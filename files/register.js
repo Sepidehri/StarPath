06090ca3299c36e2c2a24b1df38e4dcff0dc615f
Sepidehri <sepidriahi@gmail.com>
2023-06-25 12:38:47 +0200

    registeration successful message instead of redirecting to the account

--- files/register.js
+++ files/register.js
@@ -1,16 +1,19 @@
 
-document.getElementById('registration-form').addEventListener('submit', function(event) {
+
+  document.getElementById('registration-form').addEventListener('submit', function(event) {
     event.preventDefault();
-  
+
     const password = document.getElementById('password').value;
     const confirmPassword = document.getElementById('confirm-password').value;
-  
+
     if (password !== confirmPassword) {
       document.getElementById('error-message').innerText = 'Passwords do not match.';
       return;
     }
-  
-    // Perform registration logic here
-    window.location.href = 'account';
-  });
-  
+
+    
+    // Simulate a successful registration with a delay of 1 second
+    setTimeout(() => {
+      document.getElementById('registration-message').innerText = 'Registration successful!';
+    }, 1000);
+});
