b863694ab763500e5200578324ac823a54e80daf
Sepidehri <sepidriahi@gmail.com>
2023-06-26 19:02:41 +0200

    chan ge in dropdown menu

--- files/account.js
+++ files/account.js
@@ -3,7 +3,17 @@ fetch('/account', {
     method: 'GET',
     credentials: 'include', // Include cookies in the request
   })
-    .then((response) => response.json())
+    .then((response) => {
+      console.log(response); // Log the response to inspect its content
+      if (response.ok) {
+        return response.json();
+      } else if (response.status === 401) {
+        // Redirect to login page if unauthorized
+        window.location.href = 'signin';
+      } else {
+        throw new Error('Error fetching user information');
+      }
+    })
     .then((data) => {
       // Update the account information in the DOM
       const user = data.user; // Extract the user object from the response
--- files/register.js
+++ files/register.js
@@ -1,19 +1,50 @@
-
-
-  document.getElementById('registration-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    const password = document.getElementById('password').value;
-    const confirmPassword = document.getElementById('confirm-password').value;
-
-    if (password !== confirmPassword) {
-      document.getElementById('error-message').innerText = 'Passwords do not match.';
-      return;
-    }
-
-    
-    // Simulate a successful registration with a delay of 1 second
-    setTimeout(() => {
-      document.getElementById('registration-message').innerText = 'Registration successful!';
-    }, 1000);
+$(window).on("hashchange", function () {
+	if (location.hash.slice(1) == "signup") {
+		$(".page").addClass("extend");
+		$("#login").removeClass("active");
+		$("#signup").addClass("active");
+	} else {
+		$(".page").removeClass("extend");
+		$("#login").addClass("active");
+		$("#signup").removeClass("active");
+	}
 });
+$(window).trigger("hashchange");
+
+function validateLoginForm() {
+	var name = document.getElementById("logName").value;
+	var password = document.getElementById("logPassword").value;
+
+	if (name == "" || password == "") {
+		document.getElementById("errorMsg").innerHTML = "Please fill the required fields"
+		return false;
+	}
+
+	else if (password.length < 8) {
+		document.getElementById("errorMsg").innerHTML = "Your password must include atleast 8 characters"
+		return false;
+	}
+	else {
+		alert("Successfully logged in");
+		return true;
+	}
+}
+function validateSignupForm() {
+	var mail = document.getElementById("signEmail").value;
+	var name = document.getElementById("signName").value;
+	var password = document.getElementById("signPassword").value;
+
+	if (mail == "" || name == "" || password == "") {
+		document.getElementById("errorMsg").innerHTML = "Please fill the required fields"
+		return false;
+	}
+
+	else if (password.length < 8) {
+		document.getElementById("errorMsg").innerHTML = "Your password must include atleast 8 characters"
+		return false;
+	}
+	else {
+		alert("Successfully signed up");
+		return true;
+	}
+}
